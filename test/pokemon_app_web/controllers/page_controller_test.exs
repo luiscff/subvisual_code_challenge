903b3fd77dd721b26920e63bed9bc8fca9ced5f6
Luís <88602145+luiscff@users.noreply.github.com>
2024-10-15 13:03:29 +0100

    test: Implement tests for text-based search and navigation buttons

--- test/pokemon_app_web/controllers/page_controller_test.exs
+++ test/pokemon_app_web/controllers/page_controller_test.exs
@@ -1,8 +1,24 @@
 defmodule PokemonAppWeb.PageControllerTest do
   use PokemonAppWeb.ConnCase
+  use ExUnit.Case
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Search for a Pokémon"
+  end
+
+  test "GET / with a Pokémon name", %{conn: conn} do
+    conn = get(conn, ~p"/?pokemon=pikachu")
+    assert html_response(conn, 200) =~ "pikachu"
+  end
+
+  test "clicking previous button navigates to previous pokemon", %{conn: conn} do
+    # Primeira requisição: mostra o Pokémon com ID 2
+    conn = get(conn, ~p"/?pokemon=2")
+    assert html_response(conn, 200) =~ "Id: 2"
+
+    # Segunda requisição: simula clicar no botão Previous e verifica o Pokémon anterior
+    conn = get(conn, ~p"/?pokemon=1")
+    assert html_response(conn, 200) =~ "Id: 1"
   end
 end
---
+++ test/pokemon_app_web/controllers/pokemon_list_test.exs
@@ -0,0 +1,29 @@
+defmodule PokemonApp.PokemonListTest do
+  use ExUnit.Case
+
+  test "search/1 returns a list of pokemons that contains the given text" do
+
+    expected_result = [
+      "charmander",
+      "charmeleon",
+      "charizard",
+      "chimchar",
+      "charjabug",
+      "charcadet",
+      "pecharunt"
+    ]
+
+    result = PokemonApp.PokemonList.search("char")
+
+    assert result == expected_result
+  end
+
+  test "search/1 returns an empty list if no pokemon contains the given text" do
+
+    expected_result = []
+
+    result = PokemonApp.PokemonList.search("not_a_pokemon")
+
+    assert result == expected_result
+  end
+end
